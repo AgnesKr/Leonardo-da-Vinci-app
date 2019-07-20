e75035a8eb142180f6370b48487d4caf73c66c1c
AgnesKr <weltreise@wp.pl>
2019-07-20 11:54:57 +0200

    arrows working

--- main.js
+++ main.js
@@ -39,7 +39,6 @@ rightArrowElement.addEventListener("click", function () {
             tab.push(i)
         } 
         tab.reverse()
-        console.log(tab)
         for(let i = sectionElemnts.length - 1 ; i >= 0 ; i--) {
             sectionElemnts[i].style.zIndex = tab[i]
         }
@@ -63,31 +62,31 @@ rightArrowElement.addEventListener("click", function () {
         for(let i = page + 1; i < sectionElemnts.length; i++){
             sectionElemnts[i].style.zIndex = tab2[i - page]
         }
-        console.log(page)
     }
 })
 
 leftArrowElement.addEventListener("click", function () {
     if (page - 1 < 0) {
-        sectionElemnts[sectionElemnts.length - 1].style.zIndex = sectionElemnts.length - 2
-        sectionElemnts[page].style.opacity = "0"
-        page = sectionElemnts.length - 1
-        setTimeout(() => {
-            sectionElemnts[0].style.opacity = "1"
-            for (let i = sectionElemnts.length - 1; i >= 0; i--) {
-                sectionElemnts[i].style.zIndex = i
-            }
-        }, 400)
+        
     } else {
-        sectionElemnts[page].style.opacity = "0"
-        setTimeout(() => {
-            for (let i = sectionElemnts.length - 1; i >= 0; i--) {
-                sectionElemnts[i].style.zIndex = Number(sectionElemnts[i].style.zIndex) + 1
-            }
-            sectionElemnts[page].style.zIndex = 0
-            sectionElemnts[page].style.opacity = "1"
-            page = page - 1
-        }, 400)
+        page--
+        let number = page - 1,
+        tab = []
+        for(let i = page - 1; i >= 0; i--) {
+            tab.push(number)
+            number--
+        }
+        for(let i = page - 1; i >= 0; i--) {
+            sectionElemnts[i].style.zIndex = tab[i]
+            number--
+        }
+        sectionElemnts[page].style.zIndex =  "4"
+        let tab2 = []
+        for(let i = sectionElemnts.length - 1; i >= page; i--) {
+            tab2.push(i)
+        }
+        for(let i = page + 1; i < sectionElemnts.length; i++){
+            sectionElemnts[i].style.zIndex = tab2[i - page]
+        }
     }
-    console.log(page)
 })
